4cff52913733d8db14b9b995ce9576b8c85f4819
Paul Kincaid <paul.m.kincaid@gmail.com>
2015-12-05 18:28:48 -0700

    Add Comments

--- Servo-Sweep-with-Matrix/Servo-Sweep-with-Matrix.ino
+++ Servo-Sweep-with-Matrix/Servo-Sweep-with-Matrix.ino
@@ -65,23 +65,3 @@ void incrementMatrix(int L) {
   matrix.writeDisplay();
   delay(10);
 }
-//  for(pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees 
-//  {                                  // in steps of 1 degree 
-//    l_servo.write(pos);              // tell servo to go to position in variable 'pos' 
-//    delay(15);                       // waits 15ms for the servo to reach the position 
-// } 
-// for(pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees 
-//  {                                  // in steps of 1 degree 
-//    r_servo.write(pos);              // tell servo to go to position in variable 'pos' 
-//    delay(15);                       // waits 15ms for the servo to reach the position 
-//  } 
-//  for(pos = 90; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-//  {                                
-//    l_servo.write(pos);              // tell servo to go to position in variable 'pos' 
-//    delay(15);                       // waits 15ms for the servo to reach the position 
-//  } 
-//  for(pos = 90; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-//  {                                
-//    r_servo.write(pos);              // tell servo to go to position in variable 'pos' 
-//    delay(15);                       // waits 15ms for the servo to reach the position 
-//  } 
